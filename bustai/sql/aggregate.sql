de7a250b697e81bc48718d3034b4f9a142152c22
Karolis Vycius <k.vycius@gmail.com>
2017-06-20 14:30:43 +0300

    More bustai information

---
+++ bustai/sql/aggregate.sql
@@ -0,0 +1,102 @@
+DROP TABLE
+    IF EXISTS savivaldybes_su_kainom;
+DROP TABLE
+    IF EXISTS socialiniai_su_kainom;
+DROP TABLE
+    IF EXISTS Bustai;
+DROP TABLE
+    IF EXISTS KainosBeButu;
+-- Savivaldybes bustai su kainomis
+CREATE TABLE
+    savivaldybes_su_kainom AS
+SELECT
+    s.[Nr.]                                               AS Id,
+    s.[Miestas/Gyvenvietė]                                          AS Miestas,
+    s.Gatvė                                                          AS Gatve,
+    s.[Namo Nr.]                                                     AS Namas,
+    s.[Buto Nr.]                                                     AS Butas,
+    CAST(REPLACE(s.Plotas, '"', '') AS DECIMAL)                      AS Plotas,
+    k.[Patalpų nuomos]                                               AS 'Kaina',
+    k.[Patalpų nuomos] / CAST(REPLACE(s.Plotas, '"', '') AS DECIMAL) KainaM2,
+    s.Tipas,
+    k.lat,
+    k.lng
+FROM
+    savivaldybes s
+LEFT JOIN
+    kainos k
+ON
+    s.Gatvė = k.Gatvė || ' g.'
+AND s.[Namo Nr.] = k.Namas
+AND s.[Buto Nr.] = k.[Butas];
+-- Socialiniai bustai su kainomis
+CREATE TABLE
+    socialiniai_su_kainom AS
+SELECT
+    s.[Nr.]                                               AS Id,
+    s.[Miestas/Gyvenvietė]                                          AS Miestas,
+    s.Gatvė                                                          AS Gatve,
+    s.[Namo Nr.]                                                     AS Namas,
+    s.[Buto Nr.]                                                     AS Butas,
+    CAST(REPLACE(s.Plotas, '"', '') AS DECIMAL)                      AS Plotas,
+    k.[Patalpų nuomos]                                               AS 'Kaina',
+    k.[Patalpų nuomos] / CAST(REPLACE(s.Plotas, '"', '') AS DECIMAL) KainaM2,
+    s.Tipas,
+    k.lat,
+    k.lng
+FROM
+    socialiniai s
+JOIN
+    kainos k
+ON
+    s.Gatvė = k.Gatvė || ' g.'
+AND s.[Namo Nr.] = k.Namas
+AND s.[Buto Nr.] = k.[Butas];
+
+-- Sanity check
+CREATE TABLE
+    Bustai AS
+SELECT
+    *
+FROM
+    savivaldybes_su_kainom
+UNION ALL
+SELECT
+    *
+FROM
+    socialiniai_su_kainom;
+    
+-- 
+    
+CREATE TABLE
+    KainosBeButu AS
+SELECT
+    k.*
+FROM
+    kainos AS k
+LEFT JOIN
+    Bustai b
+ON
+    b.Gatve = k.Gatvė || ' g.'
+AND b.[Namas] = b.Namas
+AND b.[Butas] = b.[Butas]
+WHERE
+    b.Namas IS NULL;
+    
+-- Statictics
+SELECT
+    COUNT (*)
+FROM
+    kainos
+UNION ALL
+SELECT
+    COUNT(*)
+FROM
+    KainosBeButu
+UNION ALL
+SELECT
+    COUNT(*)
+FROM
+    Bustai
+WHERE
+    Kaina IS NULL;
